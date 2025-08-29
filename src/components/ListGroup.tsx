17c1101e59eb16d4f318810a34b2994fde99b287
nadiadp <144267049+nadiadp@users.noreply.github.com>
2025-08-28 21:53:27 -0500

    Refactor ListGroup component to accept items and heading as props; update App component to pass city list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,36 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+}
 
-  //event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+function ListGroup({ items, heading }: ListGroupProps) {
+  // state Hook for the selected index
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  // event handler
+  const handleClick = (index: number) => {
+    console.log("You clicked:", items[index]);
+    setSelectedIndex(index); // update state
+  };
 
   return (
     <>
-      <h1>List</h1>
-
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((items) => (
-          <li className="list-group-item" key={items} onClick={handleClick}>
-            {items}
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => handleClick(index)}
+          >
+            {item}
           </li>
         ))}
       </ul>
