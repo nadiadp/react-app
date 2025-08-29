5857d253c7b661a4749e58fabad905b7517b24e9
nadiadp <144267049+nadiadp@users.noreply.github.com>
2025-08-29 13:37:28 -0500

    fixed audio path

--- src/components/Mingi.tsx
+++ src/components/Mingi.tsx
@@ -28,7 +28,7 @@ function Mingi() {
       />
       <audio
         ref={audioRef}
-        src="public/waveATEEZ.mp3"
+        src="/react-app/waveATEEZ.mp3"
         //GOMBAY GOMBAYYYY
         onPlay={() => {
           if (audioRef.current) {
